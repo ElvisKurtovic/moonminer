6eef8f8b835518d34733775c49d4f0c2be911010
ElvisKurtovic <elvis.kurtovic23@gmail.com>
2021-02-15 15:24:58 -0700

    finished updated moonminer

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let cheese = 0;
 
-let multiplier = 1
+let multiplier = 1;
 
-let automult = 0
+let automult = 0;
 
 let items = {
     pickAxes: {
@@ -24,9 +24,9 @@ let items = {
 
 let automaticUpgrades = {
     mice: {
-        price: 0,
-        quantity: 1,
-        cheese: 5,
+        price: 200,
+        quantity: 0,
+        multiplier: 5,
     }
 };
 
@@ -42,6 +42,7 @@ function buyPickaxe() {
     if (cheese >= items.pickAxes.price) {
         cheese -= items.pickAxes.price
         items.pickAxes.quantity++
+        items.pickAxes.price *= 2
         equipPickaxe()
         update()
     }
@@ -51,6 +52,7 @@ function buyJackhammer() {
     if (cheese >= items.jackHammer.price) {
         cheese -= items.jackHammer.price
         items.jackHammer.quantity++
+        items.jackHammer.price *= 2
         equipJackhammer()
         update()
     }
@@ -60,6 +62,7 @@ function buyDigboi() {
     if (cheese >= items.digBoi.price) {
         cheese -= items.digBoi.price
         items.digBoi.quantity++
+        items.digBoi.price *= 2
         equipDigboi()
         update()
     }
@@ -71,6 +74,10 @@ function drawCheese() {
 
 function update() {
     document.getElementById("moonImg").innerText = cheese.toString()
+    document.getElementById("updatePCost").innerText = `${items.pickAxes.price}`
+    document.getElementById("updateJCost").innerText = `${items.jackHammer.price}`
+    document.getElementById("updateDCost").innerText = `${items.digBoi.price}`
+    document.getElementById("updateMCost").innerText = `${automaticUpgrades.mice.price}`
 }
 
 function equipPickaxe() {
@@ -97,25 +104,40 @@ function equipDigboi() {
 function equipMice() {
     let Micecount = document.getElementById("miceEquipped")
     Micecount.innerText = `${automaticUpgrades.mice.quantity}`
-    cheese += automaticUpgrades.mice.cheese * automaticUpgrades.mice.quantity
+
 
 }
 
 function collectAutoUpgrades() {
-    // if (cheese >= automaticUpgrades.mice.price) {
-    //     cheese -= automaticUpgrades.mice.price
-    //     automaticUpgrades.mice.quantity++
-    // }
-
-    cheese += automult
-    // drawCheese()
-    equipMice()
+    let autoCheese = 0
+    for (let key in automaticUpgrades) {
+        autoCheese += automaticUpgrades[key].multiplier * automaticUpgrades[key].quantity
+    }
+    cheese += autoCheese
     update()
+
+}
+
+// cheese += automult
+// drawCheese()
+// equipMice()
+// update()
+
+
+function buyMice() {
+    if (cheese >= automaticUpgrades.mice.price) {
+        cheese -= automaticUpgrades.mice.price
+        automaticUpgrades.mice.quantity++
+        automaticUpgrades.mice.price *= 2
+        equipMice()
+        update()
+    }
 }
 
 function startInterval() {
+    // collectAutoUpgrades()
     setInterval(collectAutoUpgrades, 3000);
-    // update()
+    update()
 }
 
 drawCheese()
